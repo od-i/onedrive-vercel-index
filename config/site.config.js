d52d65bce4449f3551fa908f5aa3c1bd9fb7cb82
od-i <ymrpro99987@gmail.com>
2023-03-01 00:31:42 -0800

    fix

--- config/site.config.js
+++ config/site.config.js
@@ -52,7 +52,7 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'Website',
+      name: ' 🌐Website',
       link: 'http://www.kiyoshi-subs.com/',
     },
   ],
