38b8d8b516298b0a698ecfaa6ed2eb58d9964255
od-i <126561953+od-i@users.noreply.github.com>
2023-12-24 08:01:36 +0300

    Update FolderGridLayout.tsx

--- src/components/FolderGridLayout.tsx
+++ src/components/FolderGridLayout.tsx
@@ -95,18 +95,6 @@ const FolderGridLayout = ({
               clipboard.copy(handleSelectedPermalink(getBaseUrl()))
               toast.success(t('Copied selected files permalink.'))
             }}
-          >
-            <FontAwesomeIcon icon={['far', 'copy']} size="lg" />
-          </button>
-          {totalGenerating ? (
-            <Downloading title={t('Downloading selected files, refresh page to cancel')} style="p-1.5" />
-          ) : (
-            <button
-              title={t('Download selected files')}
-              className="cursor-pointer rounded p-1.5 hover:bg-gray-300 disabled:cursor-not-allowed disabled:text-gray-400 disabled:hover:bg-white dark:hover:bg-gray-600 disabled:dark:text-gray-600 disabled:hover:dark:bg-gray-900"
-              disabled={totalSelected === 0}
-              onClick={handleSelectedDownload}
-            >
               <FontAwesomeIcon icon={['far', 'arrow-alt-circle-down']} size="lg" />
             </button>
           )}
